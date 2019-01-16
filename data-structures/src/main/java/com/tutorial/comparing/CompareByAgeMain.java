be63462b52d0d18f4d1a707f0bb51daf4958f912
falmeida <falmeida@faa-mac>
2019-01-15 23:38:07 -0500

    Update

---
+++ data-structures/src/main/java/com/tutorial/comparing/CompareByAgeMain.java
@@ -0,0 +1,34 @@
+package com.tutorial.comparing;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+public class CompareByAgeMain {
+
+	public static void main(String args[]) {
+
+		List<Student> list = new ArrayList<Student>();
+		list.add(new Student(1, "Vijay", 23));
+		list.add(new Student(2, "Ajay", 27));
+		list.add(new Student(3, "Jai", 21));
+		list.add(new Student(4, "Carlos", 20));
+		list.add(new Student(5, "John", 45));
+		list.add(new Student(6, "Ian", 24));
+		list.add(new Student(7, "Rachel", 28));
+		list.add(new Student(8, "Daniel", 35));
+		list.add(new Student(9, "Fabio", 40));
+		
+		System.out.println("Sorting by age");
+
+		Collections.sort(list, new CompareByAge());
+		Iterator<Student> iterator = list.iterator();
+		while (iterator.hasNext()) {
+			Student st = (Student) iterator.next();
+			System.out.println(st.getNumber() + " " + st.getName() + " " + st.getAge());
+		}
+
+	}
+
+}
